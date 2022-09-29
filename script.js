b8ff838a6bfcbab7dedc70a92687ca075d0080b4
Proja96 <108406302+Proja96@users.noreply.github.com>
2022-09-29 12:11:53 +0200

    sve dodato
    problem sa izborom kompjutera, kada donese jednom odluku ne bira je ponovo

--- script.js
+++ script.js
@@ -5,22 +5,78 @@ function getComputerChoice() {
     return stringRandom;
 }
 
+let player = 0;
+let pc = 0;
+
+function playRound(playerSelection, computerSelection) {
+    if (player === 5) {
+        alert('You win');
+    }
+    else if (pc === 5) {
+        alert('YOu lose');
+    }
+    else{
+        if (playerSelection === 'rock' && computerSelection === 'rock') {
+             player += 1;
+             pc += 1;
+             
+        }
+        else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
+             pc += 1;
+             console.log(player, pc);
+        }
+        else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
+             player += 1;
+             console.log(player, pc);
+        }
+        else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
+             player += 1;
+             console.log(player, pc);
+        }
+        else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper') {
+             player += 1;
+             pc += 1;
+             console.log(player, pc);
+        }
+        else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
+             pc += 1;
+             console.log(player, pc);
+        }
+        else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
+             pc += 1;
+             console.log(player, pc);
+        }
+        else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
+             player += 1;
+             console.log(player, pc);
+        }
+        else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors') {
+             player += 1;
+             pc += 1;
+             console.log(player, pc);
+        }
+    }
+ };
+let computerSelection = getComputerChoice();
 let rock = document.getElementById('rock');
+rock.addEventListener("click", () => {
+    playRound('rock', computerSelection);
+    div.textContent = 'player = ' + player + ' computer = ' + pc;
+});
+
 let paper = document.getElementById('paper');
+paper.addEventListener("click", () => {
+    playRound('paper', computerSelection);
+    div.textContent = 'player = ' + player + ' computer = ' + pc;
+});
+
 let scissors = document.getElementById('scissors');
+scissors.addEventListener("click", () => {
+    playRound('scissors', computerSelection);
+    div.textContent = 'player = ' + player + ' computer = ' + pc;
+});
 
-rock.addEventListener("click", function(e){ 
-    if (computerSelection === 'rock') {
-        console.log('1')
-    }
-    else if (computerSelection === 'paper') {
-        console.log('1')
-    }
-    else if (computerSelection === 'scissors') {
-        console.log('1')
-    }
-    
-    const computerSelection = getComputerChoice();
+let div = document.getElementById('cover');
+div.textContent = 'player = ' + player + ' computer = ' + pc;
 
-    }
- );
+
